972a72a2baf96c37c269a28be3421c11b4a66e04
hey-om7 <om.ambarkar@gmail.com>
2023-01-11 19:11:15 +0530

    commit-to-safe-code-on-cloud

---
+++ lib/MusicBox.dart
@@ -0,0 +1,96 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+class MusicBox extends StatelessWidget {
+  const MusicBox({super.key, required this.index, required this.new1});
+  final int index;
+  final int new1;
+  @override
+  Widget build(BuildContext context) {
+    return Padding(
+      padding: const EdgeInsets.all(8.0),
+      child: Container(
+        decoration: BoxDecoration(
+            image:
+                DecorationImage(image: AssetImage("assets/design$new1.png"))),
+        child: Stack(
+          children: [
+            Align(
+              alignment: Alignment.topLeft,
+              child: Padding(
+                padding: const EdgeInsets.symmetric(
+                    vertical: 15.0, horizontal: 15.0),
+                child: Text(
+                  "SESSION",
+                  style: TextStyle(
+                      fontSize: 15,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white.withOpacity(0.5)),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomLeft,
+              child: Padding(
+                padding:
+                    const EdgeInsets.symmetric(vertical: 8.0, horizontal: 8),
+                child: Text(
+                  "${index + 1}",
+                  style: TextStyle(
+                      fontSize: 60,
+                      fontWeight: FontWeight.bold,
+                      color: Colors.white),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.topRight,
+              child: GestureDetector(
+                onTap: () {
+                  print("clicked more options");
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Column(
+                    children: [
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: CircleAvatar(
+                          radius: 3,
+                          backgroundColor: Colors.white.withOpacity(0.6),
+                        ),
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(3.0),
+                        child: CircleAvatar(
+                          radius: 3,
+                          backgroundColor: Colors.white.withOpacity(0.6),
+                        ),
+                      )
+                    ],
+                  ),
+                ),
+              ),
+            ),
+            Align(
+              alignment: Alignment.bottomRight,
+              child: GestureDetector(
+                onTap: () {
+                  print("clicked play");
+                },
+                child: Padding(
+                  padding: const EdgeInsets.all(15.0),
+                  child: Image.asset(
+                    "assets/playBtn.png",
+                    width: 40,
+                  ),
+                ),
+              ),
+            )
+          ],
+        ),
+      ),
+    );
+  }
+}
---
+++ lib/home.dart
@@ -0,0 +1,100 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/src/widgets/container.dart';
+import 'package:flutter/src/widgets/framework.dart';
+
+import 'MusicBox.dart';
+
+class Home extends StatelessWidget {
+  const Home({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return SingleChildScrollView(
+      child: Column(
+        children: [
+          Stack(
+            children: [
+              Align(
+                alignment: Alignment.center,
+                child: Image.asset(
+                  "assets/backgroundDesign.png",
+                  // width: double.infinity,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomRight,
+                child: Image.asset(
+                  "assets/beginBtn.png",
+                  width: 120,
+                  height: 37,
+                ),
+              ),
+              Align(
+                alignment: Alignment.bottomLeft,
+                child: Image.asset(
+                  "assets/share.png",
+                  width: 34,
+                  height: 34,
+                ),
+              ),
+            ],
+          ),
+          Padding(
+            padding: const EdgeInsets.only(top: 35, left: 30, right: 30),
+            child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
+              children: const [
+                Text(
+                  "Deep Sleep",
+                  style: TextStyle(
+                    color: Color(0xffE4DFFE),
+                    fontSize: 24,
+                    fontWeight: FontWeight.bold,
+                  ),
+                ),
+                SizedBox(
+                  height: 10,
+                ),
+                Text(
+                  "Relax your body by getting it into a deep sleep with these mind soothing ambient tones.",
+                  style: TextStyle(
+                    color: Color(0xffA2A2CB),
+                    fontSize: 14,
+                    fontWeight: FontWeight.w300,
+                  ),
+                ),
+                SizedBox(
+                  height: 50,
+                ),
+                Text(
+                  "SLEEP MUSIC",
+                  style: TextStyle(
+                    fontSize: 12,
+                    color: Color(0xffB5B4D7),
+                    fontWeight: FontWeight.w500,
+                  ),
+                ),
+              ],
+            ),
+          ),
+          Container(
+            child: GridView.count(
+              shrinkWrap: true,
+              physics: ClampingScrollPhysics(),
+              crossAxisCount: 2,
+              children: [
+                ...List.generate(20, (index) {
+                  int _new1 = index % 4 + 1;
+                  return MusicBox(
+                    new1: _new1,
+                    index: index,
+                  );
+                }),
+              ],
+            ),
+          )
+        ],
+      ),
+    );
+  }
+}
